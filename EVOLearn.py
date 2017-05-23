d5813ebe710de2cc7f4ae58bb15236245035688f
João Monteiro <joaomonteirof@gmail.com>
2017-05-23 15:50:41 -0400

    Add files via upload

--- EVOLearn.py
+++ EVOLearn.py
@@ -1,45 +1,16 @@
 import cPickle
 import scipy.io
 
-from keras.datasets import mnist
-from keras.utils import to_categorical
-from keras import backend as K
-
 import numpy as np
 
 import models_zoo
 from Optimizer import Optimizer, DEOptimizer, NNEVO 
-from Utils import buildAndSaveModels
+from Utils import buildAndSaveModels, data_loader
 from models_zoo import MLP_MNIST
 
 ############# Import data set
 
-img_rows, img_cols = 28, 28
-num_classes = 10
-
-(x_train, y_train), (x_valid, y_valid) = mnist.load_data()
-
-if K.image_data_format() == 'channels_first':
-	x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-	x_valid = x_valid.reshape(x_valid.shape[0], 1, img_rows, img_cols)
-	input_shape = (1, img_rows, img_cols)
-else:
-	x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-	x_valid = x_valid.reshape(x_valid.shape[0], img_rows, img_cols, 1)
-	input_shape = (img_rows, img_cols, 1)
-
-
-x_train = x_train.astype('float32')
-x_valid = x_valid.astype('float32')
-x_train /= 255
-x_valid /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_valid.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = to_categorical(y_train, num_classes)
-y_valid = to_categorical(y_valid, num_classes)
+(x_train, y_train), (x_valid, y_valid) = data_loader('mnist')
 
 def main():
 
--- Utils.py
+++ Utils.py
@@ -5,8 +5,17 @@ import os
 import pickle
 import matplotlib.pyplot as plt
 from sklearn.metrics import log_loss, mean_absolute_error, mean_squared_error
+from keras import datasets
+from keras.utils import to_categorical
+from keras import backend as K
 
 class lossFuncException(Exception):
+	def __init__(self, value):
+		self.value=value
+	def __str__(self):
+		return repr(self.value)
+
+class dataSetException(Exception):
 	def __init__(self, value):
 		self.value=value
 	def __str__(self):
@@ -83,6 +92,53 @@ def plot_fitness(pkl = 'fitness.p'):
 	plt.plot(to_plot)
 	plt.legend('Fitness')	
 	plt.show()
+
+def data_loader(dataSetName):
+	try:
+		if dataSetName is 'mnist':
+			(x_train, y_train), (x_valid, y_valid) = datasets.mnist.load_data()
+			img_rows, img_cols = 28, 28
+			num_classes = 10
+		elif dataSetName is 'cifar10':
+			(x_train, y_train), (x_valid, y_valid) = datasets.cifar10.load_data()
+			img_rows, img_cols = 32, 32
+			num_classes = 10
+		elif dataSetName is 'cifar100':
+			(x_train, y_train), (x_valid, y_valid) = datasets.cifar100.load_data()
+			img_rows, img_cols = 32, 32
+			num_classes = 100
+		elif dataSetName is 'boston':
+			(x_train, y_train), (x_valid, y_valid) = datasets.boston_housing.load_data()
+			return (x_train, y_train), (x_valid, y_valid)
+		else:
+			raise Exception(lossFunction)
+
+		if K.image_data_format() == 'channels_first':
+			x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+			x_valid = x_valid.reshape(x_valid.shape[0], 1, img_rows, img_cols)
+			input_shape = (1, img_rows, img_cols)
+		else:
+			x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+			x_valid = x_valid.reshape(x_valid.shape[0], img_rows, img_cols, 1)
+			input_shape = (img_rows, img_cols, 1)
+
+
+		x_train = x_train.astype('float32')
+		x_valid = x_valid.astype('float32')
+		x_train /= 255
+		x_valid /= 255
+		print('x_train shape:', x_train.shape)
+		print(x_train.shape[0], 'train samples')
+		print(x_valid.shape[0], 'test samples')
+
+		# convert class vectors to binary class matrices
+		y_train = to_categorical(y_train, num_classes)
+		y_valid = to_categorical(y_valid, num_classes)
+
+		return (x_train, y_train), (x_valid, y_valid)
+
+	except lossFuncException:
+		print 'The required data set is not avaliable for load. Value passed:', lossFuncException.value	
 
 if __name__ == "__main__":
 	plot_fitness()
